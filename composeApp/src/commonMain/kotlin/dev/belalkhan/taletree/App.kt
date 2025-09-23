a0de9974b5685a302a71b16a06306cd0ca13dba9
Belal Khan <probelalkhan@gmail.com>
2025-09-23 17:32:53 +0530

    HomeDestination Added

--- composeApp/src/commonMain/kotlin/dev/belalkhan/taletree/App.kt
+++ composeApp/src/commonMain/kotlin/dev/belalkhan/taletree/App.kt
@@ -1,11 +1,8 @@
 package dev.belalkhan.taletree
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -14,7 +11,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import dev.belalkhan.taletree.auth.AuthView
-import dev.belalkhan.taletree.auth.AuthViewModel
 import dev.belalkhan.taletree.firebase.auth.AuthState
 import dev.belalkhan.taletree.home.HomeView
 import dev.belalkhan.taletree.splash.SplashScreen
@@ -44,9 +40,9 @@ private fun TaleTreeApp(
     val navController: NavHostController = rememberNavController()
 
     val startDestination = when (state.value) {
-        AuthState.Authenticated -> NavDestinations.Home.route
-        AuthState.Loading -> NavDestinations.Splash.route
-        AuthState.Unauthenticated -> NavDestinations.Login.route
+        AuthState.Authenticated -> RootDestination.Home.route
+        AuthState.Loading -> RootDestination.Splash.route
+        AuthState.Unauthenticated -> RootDestination.Login.route
     }
 
     MaterialTheme {
@@ -57,15 +53,15 @@ private fun TaleTreeApp(
                 modifier = Modifier.fillMaxSize()
             ) {
 
-                composable(NavDestinations.Splash.route) {
+                composable(RootDestination.Splash.route) {
                     SplashScreen()
                 }
 
-                composable(NavDestinations.Login.route) {
+                composable(RootDestination.Login.route) {
                     AuthView()
                 }
 
-                composable(NavDestinations.Home.route) {
+                composable(RootDestination.Home.route) {
                     HomeView()
                 }
             }
--- composeApp/src/commonMain/kotlin/dev/belalkhan/taletree/Nav.kt
+++ composeApp/src/commonMain/kotlin/dev/belalkhan/taletree/Nav.kt
@@ -1,7 +1,21 @@
 package dev.belalkhan.taletree
 
-sealed class NavDestinations(val route: String) {
-    object Login : NavDestinations("login")
-    object Home : NavDestinations("home")
-    object Splash : NavDestinations("splash")
+sealed class RootDestination(val route: String) {
+    object Login : RootDestination("login")
+    object Splash : RootDestination("splash")
+    object Home : RootDestination("home")
+}
+
+sealed class HomeDestination(val route: String) {
+    companion object {
+        private val PREFIX = "${RootDestination.Home.route}/"
+    }
+
+    object Dashboard : HomeDestination("${PREFIX}dashboard")
+
+    object Profile : HomeDestination("${PREFIX}profile/{userId}") {
+        fun createRoute(userId: String) = "${PREFIX}profile/$userId"
+    }
+
+    object Settings : HomeDestination("${PREFIX}settings")
 }
